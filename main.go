e2cfd5e7dc3edf037c0f1319214adc86f290a8d1
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2017-07-18 10:36:26 +1000

    Clear golint issues, apply gofmt

--- main.go
+++ main.go
@@ -10,19 +10,18 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Options struct {
+type options struct{}
+
+func makeDefaultOptions() *options {
+	return &options{}
 }
 
-func MakeDefaultOptions() (*Options) {
-	return &Options{}
-}
-
-func PrintUsage() {
+func printUsage() {
 	fmt.Printf("Usage: %s domain key [default value] [default value] ...\n", os.Args[0])
 	flag.PrintDefaults()
 }
 
-func GetTxtRecords(options *Options, domain string) ([]string, error) {
+func getTxtRecords(options *options, domain string) ([]string, error) {
 	if !strings.HasSuffix(domain, ".") {
 		domain = domain + "."
 	}
@@ -38,7 +37,7 @@ func GetTxtRecords(options *Options, domain string) ([]string, error) {
 	query.SetQuestion(domain, dns.TypeTXT)
 	query.RecursionDesired = true
 
-	response, _, err := client.Exchange(query, config.Servers[0] + ":" + config.Port)
+	response, _, err := client.Exchange(query, config.Servers[0]+":"+config.Port)
 	if err != nil {
 		return nil, errors.Wrap(err, "error executing DNS query")
 	}
@@ -47,7 +46,7 @@ func GetTxtRecords(options *Options, domain string) ([]string, error) {
 	case dns.RcodeSuccess:
 		// okay
 
-	case dns.RcodeNameError:  // a.k.a. NXDOMAIN
+	case dns.RcodeNameError: // a.k.a. NXDOMAIN
 		// TODO: add an option to allow ignoring this
 		// This is the default for safety reasons
 		return nil, errors.Errorf("no TXT records for domain %s", domain)
@@ -65,16 +64,16 @@ func GetTxtRecords(options *Options, domain string) ([]string, error) {
 	return results, nil
 }
 
-func LookUpValues(options *Options, txt_records []string, key string, default_values []string) ([]string, error) {
+func lookUpValues(options *options, txtRecords []string, key string, defaultValues []string) ([]string, error) {
 	var values []string
-	for _, record := range txt_records {
+	for _, record := range txtRecords {
 		pieces := strings.SplitN(record, "=", 2)
 		if len(pieces) > 1 && pieces[0] == key {
 			values = append(values, pieces[1])
 		}
 	}
 
-	if len(values) == 0 && len(default_values) == 0 {
+	if len(values) == 0 && len(defaultValues) == 0 {
 		// TODO: make this check optional
 		// This is just a default restriction
 		return nil, errors.Errorf("no values found for key %s, and no default provided", key)
@@ -85,35 +84,35 @@ func LookUpValues(options *Options, txt_records []string, key string, default_va
 		return nil, errors.Errorf("%d values found for key %s, but only 1 was expected", len(values), key)
 	}
 	if len(values) == 0 {
-		return default_values, nil
+		return defaultValues, nil
 	}
 	return values, nil
 }
 
 func main() {
-	options := MakeDefaultOptions()
+	options := makeDefaultOptions()
 
-	flag.Usage = PrintUsage
+	flag.Usage = printUsage
 	flag.Parse()
 
 	args := flag.Args()
 	if len(args) < 2 {
-		PrintUsage()
+		printUsage()
 		os.Exit(2)
 	}
 
 	domain := args[0]
 	key := args[1]
-	default_values := args[2:]
+	defaultValues := args[2:]
 
-	txt_records, err := GetTxtRecords(options, domain)
+	txtRecords, err := getTxtRecords(options, domain)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error doing DNS lookup:\n%+v\n", err.Error())
 		os.Exit(3)
 	}
 
 	var values []string
-	values, err = LookUpValues(options, txt_records, key, default_values)
+	values, err = lookUpValues(options, txtRecords, key, defaultValues)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error looking up values for key \"%s\" in domain %s:\n%+v\n", key, domain, err.Error())
 		os.Exit(4)
